5be1c9b4e92e27a4febdadd0f9155d71d99d2e3b
Kacper Michalski <kacperm98@gmail.com>
2019-10-24 22:18:32 +0200

    wtf

--- Component.h
+++ Component.h
@@ -8,17 +8,47 @@
 #include <string>
 #include <set>
 #include <cctype>
+#include <fstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
+
+    class Card
+    {
+    private:
+        std::string qst;
+        std::string ans;
+        int id;
+    public:
+        Card(std::string const& qst, std::string const& ans, int i)
+        {
+            this->qst = qst;
+            this->ans = ans;
+            id = i;
+        }
+        std::string getqst(){return qst;}
+        std::string getans(){return ans;}
+        int getid(){return id;}
+        void setqst(std::string &str){qst=str;}
+        void setans(std::string &str){ans=str;}
+        void setid(int i){id=i;}
+
+    };
     class Component
     {
         private:
-
+            friend class boost::serialization::access;
+            template<class Archive>
+            void serialize(Archive & ar, const unsigned int& i){};
         public:
             virtual const std::string* getname() const{return nullptr;};
             virtual void setname(std::string &str){};
             virtual void insert(Component* comp){};
             virtual const Component* operator[](int i) const {return nullptr;};
-            virtual ~Component()= default;
+            virtual ~Component() = default;
+            virtual void save() const{};
+            virtual void add(std::string const& qst, std::string const& ans){};
+            virtual Card* at(int i){};
     };
     struct alpha
     {
@@ -53,38 +83,50 @@
     class Deck: public Component
     {
         private:
-            class Card
-            {
-            private:
-                std::string qst;
-                std::string ans;
-                int id;
-            public:
-                std::string getqst(){return qst;}
-                std::string getans(){return ans;}
-                int getid(){return id;}
-                void setqst(std::string &str){qst=str;}
-                void setans(std::string &str){ans=str;}
-                void setid(int i){id=i;}
-
-            };
+            friend class boost::serialization::access;
             std::string name;
             int id;
-            std::vector<Card> cards;
+            std::vector<Card*> cards;
+            template<class Archive>
+            void serialize(Archive & ar, const unsigned int& i)
+            {
+                ar & name;
+                ar & id;
+                //ar & cards;
+            }
         public:
+
             const std::string* getname() const override{return &name;}
+            void add(std::string const& qst, std::string const& ans)
+            {
+                cards.push_back(new Card(qst, ans, cards.size()));
+            }
+            Card* at(int i)
+            {
+                if(i>=cards.size()||i<0) return nullptr;
+                return cards.at(i);
+            }
             Deck()=delete;
             Deck( std::string const &str, int i)
             {
                 name=str;
                 id=i;
             }
+
     };
     class Folder: public Component
     {
         private:
             std::set<Component*, alpha> children;
             std::string name;
+            friend class boost::serialization::access;
+            template<class Archive>
+            void serialize(Archive & ar, const unsigned int& i)
+            {
+                ar & name;
+                if(children.empty()) return;
+                //for(auto it : children) it->serialize(ar, i);
+            }
         public:
             Folder()=delete;
             const std::string* getname() const override{return &name;}
@@ -97,5 +139,6 @@
                 for(int j = 0; j<i; j++, it++);
                 return *it;
             }
+
     };
 #endif //ANKI_COMPONENT_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Component.h"
+void save(Component* comp);
 int main()
 {
 
@@ -7,6 +8,7 @@ int main()
     Component *d1 = new Deck(*new std::string("baaa"), 1), *d2 = new Deck(*new std::string("ba"), 1), *d3 = new Deck(*new std::string("aaaa"), 1);
     Component *d4 = new Deck(*new std::string(">0"), 1), *d5 = new Deck(*new std::string(">>"), 1), *d6 = new Deck(*new std::string("zzz"), 1);
     Component *f1 = new Folder(*new std::string("jjayjoker")), *f2 = new Folder(*new std::string("jjay")), *f3 = new Folder(*new std::string("aa."));
+    Component newd2;
     data.insert(d3);
     data.insert(f2);
     data.insert(d4);
@@ -20,15 +22,43 @@ int main()
     f3->insert(d2);
     f3->insert(d1);
     f3->insert(f2);
-    auto it = data.begin();
-    while(it!=data.end())
+    d1->add("dare", "him");
+    d1->add("hora", "jojo");
+    d1->add("spirited", "away");
+    d1->add("your", "name");
+    d1->add("tenki no", "ko");
+    d1->add("owaru", "made mo");
+
+
+    for(int i=0; i<6; i++)
     {
-        std::cout<<*(*it)->getname()<<std::endl;
-        it++;
+        std::cout<<d1->at(i)->getqst()<<std::endl<<d1->at(i)->getans()<<std::endl<<std::endl;
     }
-    for(int i = 0; i<5; i++)
-    {
-        std::cout << *((*f3)[i])->getname() << std::endl;
-    }
+//    auto it = data.begin();
+//    while(it!=data.end())
+//    {
+//        std::cout<<*(*it)->getname()<<std::endl;
+//        it++;
+//    }
+//    for(int i = 0; i<5; i++)
+//    {
+//        std::cout << *((*f3)[i])->getname() << std::endl;
+//    }
+
+    std::ofstream of("ra");
+    boost::archive::text_oarchive oa(of);
+    oa << (*d1);
+    delete d1;
+    std::ifstream ifs("ra");
+    boost::archive::text_iarchive ia(ifs);
+    ia>>newd2;
+    std::cout<<"------------------"<<*newd2.getname();
     return 0;
+}
+void save(Component* comp)
+{
+    if(!comp) return;
+    std::ofstream ofs(*comp->getname());
+    boost::archive::text_oarchive oa(ofs);
+    oa<<comp;
 }
